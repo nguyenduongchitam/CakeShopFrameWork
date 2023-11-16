f954b8de72c8591597ea0ca35004361231f02982
nguyenduongchitam <nguyenduongchitam@gmail.com>
2023-11-16 17:33:00 +0700

    Add project files.

---
+++ CakeShop/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CakeShop.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class HomeAdminController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult quanlytaikhoan()
+        {
+            return View();
+        }
+        public IActionResult quanlydanhmucsanpham()
+        {
+            return View();
+        }
+        public IActionResult quanlysanpham()
+        {
+            return View();
+        }
+        public IActionResult quanlyhoadon()
+        {
+            return View();
+        }
+        public IActionResult quanlykhachhang()
+        {
+            return View();
+        }
+        public IActionResult quanlyphanhoi()
+        {
+            return View();
+        }
+        public IActionResult quanlybaocaovathongke()
+        {
+            return View();
+        }
+
+    }
+}
